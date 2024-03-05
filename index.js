4061e899ff6092a8c21c60a18cee107112aa2c54
bobbyhadz <opmftw@gmail.com>
2024-03-05 08:48:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+// ✅ Remove all Styles from Element
+box.removeAttribute('style');
+
+// ✅ Remove specific style from Element
+box.style.width = null;
+
+// ✅ Remove all classes from an element
+// box.removeAttribute('class');
